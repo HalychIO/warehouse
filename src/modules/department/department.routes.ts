cef8403ee9bcdcce1365b2134efde140d38abee5
Illia <halychio3@gmail.com>
2025-09-23 21:40:09 +0300

    simplification DepartmentRoutes

--- src/modules/department/department.routes.ts
+++ src/modules/department/department.routes.ts
@@ -7,10 +7,10 @@ const departmentRoutes = Router();
 
 const controller = new DepartmentControllers();
 
-departmentRoutes.post("/", validateUtil(createDepartmentSchema), controller.create.bind(controller));
-departmentRoutes.get("/", controller.getAll.bind(controller));
-departmentRoutes.get("/:id", controller.getById.bind(controller));
-departmentRoutes.put("/:id", validateUtil(updateDepartmentSchema), controller.update.bind(controller));
-departmentRoutes.delete("/:id", controller.delete.bind(controller));
+departmentRoutes.get("/", controller.getAll);
+departmentRoutes.get("/:id", controller.getById);
+departmentRoutes.post("/", validateUtil(createDepartmentSchema), controller.create);
+departmentRoutes.put("/:id", validateUtil(updateDepartmentSchema), controller.update);
+departmentRoutes.delete("/:id", controller.delete);
 
 export default departmentRoutes;

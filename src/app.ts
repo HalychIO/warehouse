96bc0a55607604dd40988f764bc56b8b6b50a38f
Illia <halychio3@gmail.com>
2025-09-23 19:11:05 +0300

    fixed apiResponseMiddleware connection, connected Morgan and removed extra files

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,21 @@ import express from 'express';
 import departmentRoutes from "./modules/department/department.routes";
 import {errorHandlerUtil} from "./utils/errorHandler.util";
 import {apiResponseMiddleware} from "./middlewares/apiResponse.middleware";
+import morgan from "morgan";
 
 const app = express();
 
+// --- Global middlewares ---
 app.use(express.json());
+app.use(morgan("dev"));
 
-app.use("/departments", departmentRoutes);
-
+// --- Custom response middleware (додає res.api) ---
 app.use(apiResponseMiddleware);
 
+// --- Routes ---
+app.use("/departments", departmentRoutes);
+
+// --- Error handler (останній) ---
 app.use(errorHandlerUtil);
 
 export default app;

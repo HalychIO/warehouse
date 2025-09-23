65866dd5df01800c2b935c8f5cedb5e450598bc9
Illia <halychio3@gmail.com>
2025-09-23 20:59:56 +0300

    fixed validateUtil

--- src/utils/validate.util.ts
+++ src/utils/validate.util.ts
@@ -1,13 +1,14 @@
 import {z} from "zod/index";
 import {AppErrorUtil} from "./AppError.util";
+import {NextFunction, Request, Response} from "express";
 
 export default function validateUtil(schema: z.ZodSchema<any>) {
-    return (req: any, res: any, next: any) => {
+    return (req: Request, _res: Response, next: NextFunction) => {
         try {
             schema.parse(req.body);
             next();
         } catch (e: any) {
-            throw new AppErrorUtil(e.errors[0].message, 400);
+            throw new AppErrorUtil(400, e.errors[0].message);
         }
     };
 }
